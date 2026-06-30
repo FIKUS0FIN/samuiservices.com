b929c9e5c7d26292413279a298ab8d3091dcbadb
FLZ-FIKUS0FIN <dmytro.symonenko@folloze.com>
2026-06-30 12:53:02 +0700

    feat: add SEO fields to Listing and introduce database reset script

---
+++ prisma/migrations/0003_add_seo_fields.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "Listing" ADD COLUMN "galleryImages" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "services" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "priceLevel" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "mapLink" TEXT;
